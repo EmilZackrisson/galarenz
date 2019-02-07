90453f2ee71b159255f92e5f0e8322c620fc3af2
Emil Zackrisson <emil.zackrisson@gmail.com>
2019-02-07 21:24:46 +0100

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
 var attempt = 3; // Variable to count number of attempts.
-// Below function Executes on click of login button.
+
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
@@ -8,19 +8,15 @@ if ( username == "admin" && password == "adminadmin"){//FUCK YOU//FUCK YOU//FUCK
     window.location = "admin.html"; // Redirecting to other page.
     return false;
 }
-else if ( username == "tjo" && password == "tjo") {
-    window.location = "admin.html"; // Redirecting to other page.
-    return false;  
-}
 else{
-attempt --;// Decrementing by one.
-alert("You have left "+attempt+" attempt;");
-// Disabling fields after 3 attempts.
+    attempt --;     // Decrementing by one.
+    alert("You have left "+attempt+" attempt;");
+                // Disabling fields after 3 attempts.
 if( attempt == 0){
-document.getElementById("username").disabled = true;
-document.getElementById("password").disabled = true;
-document.getElementById("submit").disabled = true;
+    document.getElementById("username").disabled = true;
+    document.getElementById("password").disabled = true;
+    document.getElementById("submit").disabled = true;
 return false;
-}
-}
+        }   
+    }
 }
