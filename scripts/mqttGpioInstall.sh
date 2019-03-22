6e732c6d90d0465f25a3b91f462f0434b572ecaa
Zacke04 <emil.zackrisson@gmail.com>
2019-03-22 23:18:03 +0100

    hej

--- scripts/mqttGpioInstall.sh
+++ scripts/mqttGpioInstall.sh
@@ -4,4 +4,14 @@ pip install paho-mqtt
 
 sudo apt-get install python-dev python-rpi.gpio
 
+wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key
 
+sudo apt-key add mosquitto-repo.gpg.key
+
+cd /etc/apt/sources.list.d/
+
+sudo wget http://repo.mosquitto.org/debian/mosquitto-wheezy.list
+
+sudo apt-get update
+
+sudo apt-get install mosquitto
