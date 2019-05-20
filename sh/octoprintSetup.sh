8eeea2215276a34f287083df0b72c7bcaed16643
Emil Zackrisson <emil.zackrisson@gmail.com>
2019-05-20 18:56:46 +0200

    Create octoprintSetup.sh

---
+++ sh/octoprintSetup.sh
@@ -0,0 +1,24 @@
+cd ~
+sudo apt update
+sudo apt install python-pip python-dev python-setuptools python-virtualenv git libyaml-dev build-essential
+mkdir OctoPrint && cd OctoPrint
+virtualenv venv
+source venv/bin/activate
+pip install pip --upgrade
+pip install https://get.octoprint.org/latest
+
+sudo usermod -a -G tty pi
+sudo usermod -a -G dialout pi
+
+wget https://github.com/foosel/OctoPrint/raw/master/scripts/octoprint.init && sudo mv octoprint.init /etc/init.d/octoprint
+wget https://github.com/foosel/OctoPrint/raw/master/scripts/octoprint.default && sudo mv octoprint.default /etc/default/octoprint
+sudo chmod +x /etc/init.d/octoprint
+
+cd ~
+sudo apt install subversion libjpeg62-turbo-dev imagemagick ffmpeg libv4l-dev cmake
+git clone https://github.com/jacksonliam/mjpg-streamer.git
+cd mjpg-streamer/mjpg-streamer-experimental
+export LD_LIBRARY_PATH=.
+make
+
+sudo service octoprint start
